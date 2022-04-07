a965483a7bc19a204135a9c9115e8cc9990d57a2
MaruAbrar <maracode1@gmail.com>
2022-04-07 07:25:50 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *main - main block
+ *
+ *Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
