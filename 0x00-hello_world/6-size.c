1f46db00b8f8378021fce9d756205bf49b6d710d
MaruAbrar <maracode1@gmail.com>
2022-04-07 07:28:23 -0700

    6-task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *main - main block
+ *Return: 0
+ */
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
