36605a40ed99745bff3dcc32270879f97f4bb76d
MarufAbrar <maracode1@gmail.com>
2022-05-26 12:10:38 +0300

    justsimpleplus

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD =/tmp/nrandom.so
